35de3b8e83a262587fd491ac0f37b057c704c33d
okyrahmanto <oky.rahmanto@gmail.com>
2021-06-29 07:08:13 +0800

    fixing interactive docker

--- build-docker.sh
+++ build-docker.sh
@@ -9,7 +9,9 @@ fi
 GOOS=$OS GOARCH=$ARCHITECTURE go build -o build/agent-things .
 docker build --build-arg USER_ID=$(id -u) --build-arg GROUP_ID=$(id -g) --no-cache -t agent-things .
 if [ ! -z "$1" ]; then    
-    docker run -it --rm -p $1:10000 --network="first-network" --name="agent1-device" agent-things:latest 
+    
+    docker create --rm -p $1:10000 --network="first-network" --name=agent1-device agent-things:latest 
     docker network connect node agent1-device
+    docker start -i agent1-device
 fi
 
