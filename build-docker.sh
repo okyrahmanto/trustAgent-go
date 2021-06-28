46406c07b0fbfe515dc03554d2a1682e6bed59e6
okyrahmanto <oky.rahmanto@gmail.com>
2021-06-29 06:34:47 +0800

    add node network

--- build-docker.sh
+++ build-docker.sh
@@ -9,6 +9,6 @@ fi
 GOOS=$OS GOARCH=$ARCHITECTURE go build -o build/agent-things .
 docker build --build-arg USER_ID=$(id -u) --build-arg GROUP_ID=$(id -g) --no-cache -t agent-things .
 if [ ! -z "$1" ]; then    
-    docker run -it --rm -p $1:10000 --network="first-network" agent-things:latest 
+    docker run -it --rm -p $1:10000 --network="first-network" --network="node" agent-things:latest 
 fi
 
